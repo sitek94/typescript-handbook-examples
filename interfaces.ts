90568ab871c955ee9cd3290b0282a117ab87ea66
sitek94 <msitkowski94@gmail.com>
2021-02-08 11:28:43 +0100

    readonly properties

--- interfaces.ts
+++ interfaces.ts
@@ -55,3 +55,42 @@ function createSquare(config: SquareConfig): { color: string; area: number } {
 }
 
 console.log(createSquare({ color: "black" }));
+
+/**
+ *
+ * Readonly properties
+ *
+ */
+
+// Objects
+interface Point {
+  readonly x: number;
+  readonly y: number;
+}
+
+let p1: Point = { x: 10, y: 20 };
+// p1.x = 5; // ❌ Error
+
+// Arrays
+let a: number[] = [1, 2, 3, 4];
+let ro: ReadonlyArray<number> = a;
+
+
+// ro[0] = 12;      // ❌ Error
+// ro.push(5);      // ❌ Error
+// ro.length = 100; // ❌ Error
+// a = ro;          // ❌ Error
+
+// Even this is illegal:
+// a = ro; // ❌ Error
+
+// However we can overwrite it with type assertion
+a = ro as number[];
+
+/**
+ * Readonly vs const.
+ * 
+ * `const` for variables
+ * 
+ * `readonly` for properties 
+ */
