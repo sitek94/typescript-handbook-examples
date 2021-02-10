f410ddf2b01d8640405ba9de996c6fb5e0cadb12
sitek94 <msitkowski94@gmail.com>
2021-02-10 14:32:18 +0100

    recap

---
+++ functions.ts
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////
+// RECAP
+///////////////////////////////////////////////////////
+
+// Named function
+function namedFunc() {
+  console.log(`I'm named function`);
+}
+
+// Anonymous function
+let anonymFunc = function () {
+  console.log(`I'm anonymous function`);
+};
+
