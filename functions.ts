570a7ef2f8dc41892375ea2a7eaa923e5e4c46c6
sitek94 <msitkowski94@gmail.com>
2021-02-11 12:38:59 +0100

    Optional and Default Parameters

--- functions.ts
+++ functions.ts
@@ -50,3 +50,47 @@ let myAdd = function (x: number, y: number): number {
 let myAdd2: (baseValue: number, increment: number) => number = function (x, y) {
   return x + y;
 };
+
+///////////////////////////////////////////////////////
+// Optional and Default Parameters
+///////////////////////////////////////////////////////
+{
+  function buildName(firstName: string, lastName: string) {
+    return firstName + " " + lastName;
+  }
+
+  // let result1 = buildName("Bob"); // ❌ error, too few parameters
+  // let result2 = buildName("Bob", "Adams", "Sr."); // ❌ error, too many parameters
+  let result3 = buildName("Bob", "Adams"); // ah, just right
+}
+
+{
+  function buildName(firstName: string, lastName?: string) {
+    if (lastName) return firstName + " " + lastName;
+    return firstName;
+  }
+  let result1 = buildName("Bob"); // works correctly now
+  // let result2 = buildName("Bob", "Adams", "Sr."); // ❌ error, too many parameters
+  let result3 = buildName("Bob", "Adams"); // ah, just right
+}
+
+{
+  function buildName(firstName: string, lastName = "Smith") {
+    if (lastName) return firstName + " " + lastName;
+  }
+  let result1 = buildName("Bob"); // works correctly now
+  let result2 = buildName("Bob", undefined); // works correctly now
+  // let result3 = buildName("Bob", "Adams", "Sr."); // ❌ error, too many parameters
+  let result4 = buildName("Bob", "Adams"); // ah, just right
+}
+
+{
+  function buildName(firstName = "Will", lastName: string) {
+    return firstName + " " + lastName;
+  }
+  
+  // let result1 = buildName("Bob"); // ❌ error, too few parameters
+  // let result2 = buildName("Bob", "Adams", "Sr."); // ❌ error, too many parameters
+  let result3 = buildName("Bob", "Adams"); // okay and returns "Bob Adams"
+  let result4 = buildName(undefined, "Adams"); // okay and returns "Will Adams"
+}
